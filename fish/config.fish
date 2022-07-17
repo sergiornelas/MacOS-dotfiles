66681fcd2f735bdd631ff55bd8ba309b073aa484
sergiornelas <sergio.ornelas.92@outlook.com>
2022-07-17 02:15:33 -0500

    switched to wezterm

--- fish/config.fish
+++ fish/config.fish
@@ -3,14 +3,6 @@ and not set -q TMUX
   exec tmux
 end
 
-# Alias is just using eval to make a function anyway. It also
-# seems to call out to sed, so that might be why it's slow.
-# I tend to think alias should be removed from fish anyway.
-# For optimal startup time you should use functions and make them lazy-loaded
-# which you can do interactively with funced and funcsave,
-# or manually by saving each function in its own file
-# ~/.config/fish/functions/name-of-function.fish
-
 # FZF
 export FZF_DEFAULT_OPTS='--bind ctrl-o:up,ctrl-i:down,ctrl-j:accept --layout=reverse'
 
@@ -71,3 +63,11 @@ end
 ## eliminate fish_prompt:
 # set -U fish_greeting ""
 # fish_prompt
+
+# Alias is just using eval to make a function anyway. It also
+# seems to call out to sed, so that might be why it's slow.
+# I tend to think alias should be removed from fish anyway.
+# For optimal startup time you should use functions and make them lazy-loaded
+# which you can do interactively with funced and funcsave,
+# or manually by saving each function in its own file
+# ~/.config/fish/functions/name-of-function.fish
