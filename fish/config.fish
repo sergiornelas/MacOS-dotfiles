45a8d641eb94313ba1ab1602eacbabad14976a74
sergiornelas <sergio.ornelas.92@outlook.com>
2022-09-13 22:04:07 -0500

    fish reinstalled, caps tap recent window

--- fish/config.fish
+++ fish/config.fish
@@ -1,63 +1,21 @@
 # FZF
-export FZF_DEFAULT_OPTS='--bind ctrl-o:up,ctrl-i:down,ctrl-j:accept --layout=reverse'
+export FZF_DEFAULT_OPTS='--bind ctrl-o:up,ctrl-i:down,ctrl-j:accept,ctrl-d:half-page-down,ctrl-u:half-page-up --layout=reverse'
 
 # Default editor
 set -gx EDITOR nvim
-# Default browser
-set -gx BROWSER brave
 
-# You have to open terminal twice to apply
+# Node default version (you have to open terminal twice to apply)
 set --universal nvm_default_version 14
 
-function _peco_change_directory
-    if [ (count $argv) ]
-        peco --layout=top-down --query "$argv " | perl -pe 's/([ ()])/\\\\$1/g' | read foo
-    else
-        peco --layout=bottom-up | perl -pe 's/([ ()])/\\\\$1/g' | read foo
-    end
-    if [ $foo ]
-        builtin cd $foo
-        commandline -r ''
-        commandline -f repaint
-    else
-        commandline ''
-    end
-end
+# Fish_prompt:
+set -U fish_greeting ""
 
-function peco_change_directory
-    begin
-        echo $HOME/.config
-        # ghq list -p
-        ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
-        # ls -ad $HOME/Developments/*/* |grep -v \.git
-        ls -ad $HOME/* | grep -v \.git
-    end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
-end
+# Default browser
+# set -gx BROWSER brave
 
-function peco_select_history
-    if test (count $argv) = 0
-        set peco_flags --layout=bottom-up
-    else
-        set peco_flags --layout=top-down --query "$argv"
-    end
-    history | peco $peco_flags | read foo
-    if [ $foo ]
-        commandline $foo
-    else
-        commandline ''
-    end
-end
-
-# brew install fd (styled find, optional)
-# brew install peco
-function fish_user_key_bindings
-    bind \cr 'peco_select_history (commandline -b)'
-    bind \co peco_change_directory
-end
-
-## eliminate fish_prompt:
-# set -U fish_greeting ""
-# fish_prompt
+# Default shell in macOS:
+# Terminal/Preferences/General/Shells open with: Command (complete path):
+# /usr/local/bin/fish
 
 # Alias is just using eval to make a function anyway. It also
 # seems to call out to sed, so that might be why it's slow.
