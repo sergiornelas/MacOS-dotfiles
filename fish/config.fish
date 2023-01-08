abfe82677790b1d0088ac664baa4a4b35c08328f
sergiornelas <sergio.ornelas.92@outlook.com>
2023-01-07 21:49:10 -0600

    macbook 16inches migration, .gitconfig cleaned

--- fish/config.fish
+++ fish/config.fish
@@ -36,10 +36,6 @@ set -U fish_greeting ""
 # Default browser
 # set -gx BROWSER brave
 
-# Default shell in macOS:
-# Terminal/Preferences/General/Shells open with: Command (complete path):
-# /usr/local/bin/fish
-
 # Alias is just using eval to make a function anyway. It also
 # seems to call out to sed, so that might be why it's slow.
 # I tend to think alias should be removed from fish anyway.
