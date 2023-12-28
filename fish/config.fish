a1641495b887cbe42b25e806bc72af04c023ab13
sergiornelas <sergio.ornelas.92@outlook.com>
2023-12-27 23:43:32 -0600

    karabiner: spanish keys support, alfred keymaps, hyper switch app, yabai remaped

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 # FZF
-export FZF_DEFAULT_OPTS='--bind ctrl-o:up,ctrl-i:down,ctrl-j:accept,ctrl-n:toggle+down,ctrl-p:toggle+up,ctrl-m:preview-down,ctrl-k:preview-up  --layout=reverse'
+export FZF_DEFAULT_OPTS='--bind ctrl-o:up,ctrl-i:down,ctrl-j:accept,ctrl-n:toggle+down,ctrl-p:toggle+up,ctrl-m:preview-down,ctrl-k:preview-up,ctrl-g:toggle-all,ctrl-z:next-history,ctrl-x:prev-history --layout=reverse'
 
 # FZF fish
 fzf_configure_bindings --variables
