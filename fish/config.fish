a24a1c8c1088ca7baff8dd18d756badc87297e59
sergiornelas <sergio.ornelas.92@outlook.com>
2024-01-02 02:13:18 -0600

    deleted fzf_fish plugin

--- fish/config.fish
+++ fish/config.fish
@@ -1,11 +1,11 @@
 # FZF
 export FZF_DEFAULT_OPTS='--bind ctrl-o:up,ctrl-i:down,ctrl-j:accept,ctrl-n:toggle+down,ctrl-p:toggle+up,ctrl-m:preview-down,ctrl-k:preview-up,ctrl-g:toggle-all,ctrl-z:next-history,ctrl-x:prev-history --layout=reverse'
 
-# FZF fish
-fzf_configure_bindings --variables
-
 # Default editor
 set -gx EDITOR nvim
+
+# Map ctrl-o for up search
+bind \co 'up-or-search'
 
 # Node default version (you have to open terminal twice to apply)
 set --universal nvm_default_version 18.16.0
@@ -15,26 +15,6 @@ set -U fish_greeting ""
 
 # Rust
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-
-# FEATURE            |  MNEMONIC KEY SEQUENCE        |  CORRESPONDING OPTION
-# Search directory   |  Ctrl+Alt+F (F for file)      |  --directory
-# Search git log     |  Ctrl+Alt+L (L for log)       |  --git_log
-# Search git status  |  Ctrl+Alt+S (S for status)    |  --git_status
-# Search history     |  Ctrl+R     (R for reverse)   |  --history
-# Search variables   |  Ctrl+V     (V for variable)  |  --variables
-# Search processes   |  Ctrl+Alt+P (P for process)   |  --processes
-
-# EXAMPLES
-# Install the default mnemonic bindings
-#   $ fzf_configure_bindings
-# Install the default bindings but override git log's binding to Ctrl+G
-#   $ fzf_configure_bindings --git_log=\cg
-# Install the default bindings but leave search history unbound
-#   $ fzf_configure_bindings --history
-# Alternative style of disabling search history
-#   $ fzf_configure_bindings --history=
-# An agglomeration of all the options
-#   $ fzf_configure_bindings --git_status=\cg --history=\ch --variables --directory --git_log
 
 # Default browser
 # set -gx BROWSER brave
