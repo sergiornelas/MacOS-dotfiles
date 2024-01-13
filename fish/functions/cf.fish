05dfd69d6e2d7580303eda6b4b3e31319cb5a1b2
sergiornelas <sergio.ornelas.92@outlook.com>
2024-01-13 14:09:09 -0600

    alias update config

--- fish/functions/cf.fish
+++ fish/functions/cf.fish
@@ -1,3 +1,3 @@
-function conf --wraps='nvim' --description 'alias conf=config'
+function cf --wraps='nvim' --description 'alias cf=config'
   cd ~/.config/ && kitty @set-tab-title .config && nvim && kitty @set-tab-title
 end
