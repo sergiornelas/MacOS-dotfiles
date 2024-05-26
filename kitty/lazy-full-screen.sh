cfd66ab925f49a0123aff71d0985e40fda47f4a3
sergiornelas <sergio.ornelas.92@outlook.com>
2024-05-25 21:40:33 -0600

    polished lazygit command

--- kitty/lazy-full-screen.sh
+++ kitty/lazy-full-screen.sh
@@ -2,9 +2,14 @@
 	
 HAS_ZOOM=$(yabai -m query --windows --window | jq -r '."has-parent-zoom"')
 
+function lazygit_with_post_command() {
+  lazygit
+  yabai -m window --toggle zoom-fullscreen
+}
+
 if [[ $HAS_ZOOM = "true" ]]; then
   lazygit
   exit 0
 fi
 
-yabai -m window --toggle zoom-fullscreen && lazygit
+yabai -m window --toggle zoom-fullscreen && lazygit_with_post_command
