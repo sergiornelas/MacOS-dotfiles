f32c0909753cca69686bf03652c7b44c837e1b16
sergiornelas <sergio.ornelas.92@outlook.com>
2026-04-26 16:56:08 -0600

    harley sessions, toggle claude code

--- kitty/toggle_term.py
+++ kitty/toggle_term.py
@@ -3,22 +3,23 @@ from kittens.tui.handler import result_handler
 def main(args):
     pass
 
-def toggle_term(boss):
+def toggle_term(boss, window):
     tab = boss.active_tab
 
     all_another_wins = tab.all_window_ids_except_active_window
     have_only_one = len(all_another_wins) == 0
 
     if have_only_one:
-        active_window = boss.active_window
-        cwd = active_window.cwd_of_child if active_window else "~"
-
-        tab.goto_layout("tall")
-        boss.call_remote_control(
-            active_window,
-            ("launch", f"--cwd={cwd}", "--type=window", "opencode", "--port")
-        )
-        tab.neighboring_window("right")
+        # active_window = boss.active_window
+        # cwd = active_window.cwd_of_child if active_window else "~"
+        # tab.goto_layout("tall")
+        # boss.call_remote_control(
+        #     active_window,
+        #     ("launch", f"--cwd={cwd}", "--type=window", "opencode", "--port")
+        # )
+        # tab.neighboring_window("right")
+        window.write_to_child("\x1b:ClaudeCode\r")
+        return
     else:
         if tab.current_layout.name == "stack":
             tab.last_used_layout()
@@ -38,4 +39,4 @@ def handle_result(args, result, target_window_id, boss):
     window = boss.window_id_map.get(target_window_id)
     if window is None:
         return
-    toggle_term(boss)
+    toggle_term(boss, window)
